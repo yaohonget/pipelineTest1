b8ce7fcbd9388a85b1efae1f8e875f2e35e13f69
Hong Yao <yaohonget@gmail.com>
2025-07-16 17:31:16 +0200

    update workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,24 +9,32 @@ resource "aws_ecr_repository" "app" {
 }
 
 # 创建 IAM 角色用于 EKS
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eksClusterRole"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-    }]
-  })
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "eks-AmazonEKSClusterPolicy" {
-  role       = aws_iam_role.eks_cluster_role.name
+resource "aws_iam_role" "eks_cluster_role" {
+  name               = "eksClusterRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_cluster_role.name
 }
 
 # 创建 VPC + 子网 + 互联网网关（使用模块）
